8ac36ace8e0666e0a8d6c0ff333f1e287ea909e2
Marco Sussitz <sussitzmarco@gmail.com>
2025-01-30 14:53:46 +0100

    update presentation

--- presentation/hotswapAgent/components/Counter.vue
+++ presentation/hotswapAgent/components/Counter.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const props = defineProps({
-  count: {
-    default: 0,
-  },
-})
+const output = ref('');
+const intervalId = ref<number | null>(null);
 
-const counter = ref(props.count)
+function fetchOutput() {
+    fetch('http://localhost:8081/output')
+        .then(response => response.text())
+        .then(data => {
+            output.value = data;
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+function startFetching() {
+    if (!intervalId.value) {
+        fetchOutput(); // Initial fetch
+        intervalId.value = setInterval(fetchOutput, 1000); // Refresh every second
+    }
+}
+
+function stopFetching() {
+    if (intervalId.value) {
+        clearInterval(intervalId.value);
+        intervalId.value = null;
+    }
+}
 </script>
 
 <template>
@@ -18,20 +36,26 @@ const counter = ref(props.count)
       font="mono"
       outline="!none"
       hover:bg="gray-400 opacity-20"
-      @click="counter -= 1"
+      bg="green-500"
+      text="white"
+      @click="startFetching"
     >
-      -
+      Run
     </button>
-    <span m="auto" p="2">{{ counter }}</span>
     <button
       border="l main"
       p="2"
       font="mono"
       outline="!none"
       hover:bg="gray-400 opacity-20"
-      @click="counter += 1"
+      bg="red-500"
+      text="white"
+      @click="stopFetching"
     >
-      +
+      Stop
     </button>
+  </div>
+  <div class="pt-4">
+    <textarea v-model="output" rows="10" cols="50" readonly></textarea>
   </div>
 </template>
---
+++ src/test/java/test.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+
+public class test {
+
+    @Test
+    public  void testSomething() {
+        List<String> test = mock();
+    }
+}
