14844aa2202ee4a024d98997fa25bfa1f6f3ca16
Marco Sussitz <sussitzmarco@gmail.com>
2025-02-04 12:25:54 +0100

    command for startup

--- codeRunner/src/main/java/micronaut/Application.java
+++ codeRunner/src/main/java/micronaut/Application.java
@@ -4,6 +4,7 @@ import io.micronaut.runtime.Micronaut;
 
 public class Application {
 
+    //http://localhost:8081/start
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
     }
